bf0233d8b64cc4b9f5f4fd28fef27666f85308aa
MPeezy <marjoriepatton@rocketmortgage.com>
2022-01-05 08:06:27 -0500

    initial

---
+++ Cards/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Cards.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cards.Controllers
+{
+    public class HomeController : Controller
+    {
+       CardsDAL CARDS = new CardsDAL();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Card()
+        {
+            CardsSearchResults csr = new CardsSearchResults();
+            csr = CARDS.GetCard(1);
+            return View(csr);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cards/Models/CardsDAL.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Cards.Models
+{
+    public class CardsDAL
+    {
+        public Card GetCard(int deck_id)
+        {
+            //First step is to put in your Endpoint, your URL 
+            string url = "https://deckofcardsapi/" + deck_id;
+
+            //Next we need to create our request 
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            //Next if your API needs any kind of login or key, that may go here 
+            //SWAPI doesn't need anything so we're good
+
+            //Now we're ready to send off our request and grab the server's response 
+            //Inside our response, the resulting data lives 
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Pull the result into a stream reader which will then give us a string 
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            //Grab our response string - read to end starts at the top of our response file and returns each line 
+            //until it hits the end. 
+            string result = rd.ReadToEnd();
+
+            //This line converts our JSON string into a person object automatically 
+            Card c = JsonConvert.DeserializeObject<Card>(result);
+
+            //Later we'll convert our string into a model which makes it much easier to use for .net
+            return c;
+        }
+
+
+
+
+        public Card GetCards(int deck_id)
+        {
+            //First step is to put in your Endpoint, your URL 
+            string url = "https://deckofcardsapi/" + deck_id;
+
+            //Next we need to create our request 
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            //Next if your API needs any kind of login or key, that may go here 
+            //SWAPI doesn't need anything so we're good
+
+            //Now we're ready to send off our request and grab the server's response 
+            //Inside our response, the resulting data lives 
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Pull the result into a stream reader which will then give us a string 
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            //Grab our response string - read to end starts at the top of our response file and returns each line 
+            //until it hits the end. 
+            string result = rd.ReadToEnd();
+
+            //This line converts our JSON string into a person object automatically 
+            Card c = JsonConvert.DeserializeObject<Card>(result);
+
+            //Later we'll convert our string into a model which makes it much easier to use for .net
+            return c;
+        }
+
+
+
+
+
+
+    }
+}
